02a851bd1e8471d50844f7d02cff870ffd7443bd
Ola Vea <ola@olavea.com>
2019-08-06 07:29:10 +0200

    initial commit #1

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,19 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `@olavea/gatsby-theme-picture-book`,
+      options: {
+        basePath: `/`,
+        imagePath: `book/images`,
+
+        infoLink: {
+          title:
+            "Build a book with a child and this gatsby-theme-picture-book. Or give us a pull request at github.",
+          url: `https://github.com/olavea/gatsby-theme-picture-book`,
+        },
+      },
+    },
+  ],
 }
